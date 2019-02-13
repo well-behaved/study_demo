1f10ab76f4bb384449f06e7603a7ece30d5622cb
“xuexiong” <xuexiong@souche.com>
2019-02-13 10:12:35 +0800

    阶段性提交

--- springStudy/src/test/java/com/xue/SpringIocTest.java
+++ springStudy/src/test/java/com/xue/SpringIocTest.java
@@ -15,4 +15,27 @@ public class SpringIocTest {
     public void test(){
         System.out.println(WorkflowTypeEnums.APPLY_ALTER_PRICE);
     }
+
+    @Test
+    public void test2(){
+        //排序略
+        Integer[] nums = {-111,-11,-4,-1,-1,0, 1, 2,9,23,111};
+        Integer begin = 0;
+        Integer end = nums.length-1;
+        while(begin<end){
+            Integer first = nums[begin];
+            Integer thrid = nums[end];
+            end--;
+            while (begin<end){
+                Integer second =nums[end];
+//                System.out.println(first.toString()+thrid.toString()+second.toString()+"--");
+                if(first+thrid+second==0){
+                    System.out.println(first.toString()+"***"+second.toString()+"***"+thrid.toString());
+                }
+                end--;
+            }
+            begin++;
+            end=nums.length-1;
+        }
+    }
 }
