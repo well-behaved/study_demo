7a664879985142bb7c99cfae74ed32bcc6e94348
“xuexiong” <xuexiong@souche.com>
2019-03-08 11:37:54 +0800

    netty begin

---
+++ nettyclient/src/main/java/com/xue/nettyclient/Main.java
@@ -0,0 +1,12 @@
+package com.xue.nettyclient;
+
+/**
+ * @Auther: xuexiong@souche.com
+ * @Date: 2019/3/6 19:01
+ * @Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ nettyserver/src/main/java/com/xue/nettyserver/Main.java
@@ -0,0 +1,9 @@
+package com.xue.nettyserver;
+
+/**
+ * @Auther: xuexiong@souche.com
+ * @Date: 2019/3/8 09:49
+ * @Description:
+ */
+public class Main {
+}
