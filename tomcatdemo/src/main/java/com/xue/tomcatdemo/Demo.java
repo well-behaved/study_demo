63790abe0d010e2fc0bc990f087d6298d996d0d4
“xuexiong” <xuexiong@souche.com>
2019-05-07 18:26:01 +0800

    tomat提交

--- tomcatdemo/src/main/java/com/xue/tomcatdemo/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.xue.tomcatdemo;
-
-/**
- * @Auther: xuexiong@souche.com
- * @Date: 2019/5/7 17:08
- * @Description:
- */
-public class Demo {
-}
--- tomcatdemo/src/main/java/com/xue/tomcatdemo/ex20/pyrmont/standardmbeantest/CarMBean.java
+++
@@ -1,8 +0,0 @@
-package com.xue.tomcatdemo.ex20.pyrmont.standardmbeantest;
-public interface CarMBean {
-
-  public String getColor();
-  public void setColor(String color);
-  public void drive();
-
-}
